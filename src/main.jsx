0da95f81dc848c8d599ce248533476ade869e430
chethanavirajini <cheth.virajini@gmail.com>
2023-12-15 13:46:13 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './index.css'
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense('ORg4AjUWIQA/Gnt2VlhiQlVPd0BDWXxLflFyVWJbdVxwfldOcC0sT3RfQF5iS39WdkVhWn5fdHZRQA==');
 import { BrowserRouter } from 'react-router-dom';
 import { ContextPrvide } from './contexts/ContextProvide'
 
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent,ColumnsDirective,  ColumnDirective, Page,Selection, Inject,Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import Header from '../components/Header'
+import {customersData, customersGrid} from '../data/Data'
+
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div>
+      <Header category="Page" title = "Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete']}
+        editSettings={{allowDeleting: true, allowEditing: true}}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index)=>(
+            <ColumnDirective key={index} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort]}/>
+      </GridComponent>
+    </div>
   )
 }
 
